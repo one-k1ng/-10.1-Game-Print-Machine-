9d32fedf4889ec04f4063853da5f5ecce91d4ff9
Администратор <@DESKTOP-PHORFLN>
2020-11-03 18:53:19 +0300

    Добавьте файлы проекта.

---
+++ Stats.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика10._1_Game_Print_Machine_
+{
+    class Stats
+    {
+        public int Total = 0;
+        public int Missed = 0;
+        public int Correct = 0;
+        public int Accuracy = 0;
+
+        public void Update(bool correctKey)
+        {
+            Total++;
+            if (!correctKey) Missed++;
+            else Correct++;
+
+            Accuracy = 100 * Correct / (Missed + Correct);
+        }
+    }
+}
